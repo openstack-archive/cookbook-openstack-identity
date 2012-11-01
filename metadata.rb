f97b7fae31397fc212b9e84c768317ab4347e319
mattray <matthewhray@gmail.com>
2012-11-01 11:07:04 -0500

    versioned to match OpenStack releases

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "matt@opscode.com"
 license          "Apache 2.0"
 description      "The OpenStack Identity service Keystone."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "5.0.0"
+version          "2012.1.0"
 
 %w{ ubuntu fedora }.each do |os|
   supports os
