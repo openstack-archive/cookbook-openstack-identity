46757893c65de0039d1577ad013d63da0ffa906b
John Dewey <john@dewey.ws>
2013-01-03 21:37:45 -0800

    Added name attribute to top of metadata

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,14 @@
+name             "keystone"
 maintainer       "Opscode, Inc."
 maintainer_email "matt@opscode.com"
 license          "Apache 2.0"
 description      "The OpenStack Identity service Keystone."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "2012.2.0"
-name             "keystone"
 
 %w{ ubuntu fedora }.each do |os|
   supports os
-end
+endt
 
 recipe           "keystone::db", "Configures database for use with keystone"
 recipe           "keystone::server", "Installs and Configures Keystone Service"
