6208a34946f2a1585a9e0ba010f6d255fb3a2093
Jan Klare <j.klare@cloudbau.de>
2016-05-31 10:31:59 +0200

    initial commit for the newton development cycle
    Change-Id: I2e9c4e159076df49dae5d5cae2e64afa5835373b

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer_email 'openstack-dev@lists.openstack.org'
 license 'Apache 2.0'
 description 'The OpenStack Identity service Keystone.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '13.0.0'
+version '14.0.0'
 
 %w(ubuntu redhat centos).each do |os|
   supports os
 end
 
 depends 'apache2', '~> 3.1'
-depends 'openstack-common', '>= 13.0.0'
+depends 'openstack-common', '>= 14.0.0'
